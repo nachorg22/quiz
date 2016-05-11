9995e05d7a652d14c6ff6fa0a4ebbcf01b23ff1f
nachorg22 <ignaciorg22@gmail.com>
2016-05-11 18:46:34 +0200

    Tema 13: Mensajes flash

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -97,9 +97,11 @@ exports.create = function(req, res, next) {
 //guarda en DB los campos pregunta y respuesta de quiz
 	quiz.save({fields: ["question", "answer"]})
 		.then(function(quiz){
+			req.flash('success', 'Quiz creado con éxito.');
 			res.redirect('/quizzes');	//res.redirect:
 		})								//Redirección HTTP a lista de preguntas
 		.catch(function(error){
+			req.flash('error', 'Error al crear un Quiz: ' + error.message);
 			next(error);
 		});
 };
